608fb0398eb9009440dff9f43319f274c3227eaa
Dylan Brennan <brennand@wit.edu>
2018-04-13 18:23:42 -0400

    die files i dont understand

--- src/Player.java
+++ src/Player.java
@@ -105,4 +105,6 @@ public class Player
         x=x+10;
     }
 
+    
+
 }
--- src/Zone.java
+++ src/Zone.java
@@ -1,4 +1,27 @@
+import java.awt.Image;
+import java.util.ArrayList;
 
 public class Zone {
-
+	Image img;
+	int defaultx;
+	int defaulty;
+	ArrayList<Integer> locations = new ArrayList<Integer>();
+	Zone next;
+	public void setImg(Image imgIn)
+	{
+		img = imgIn;
+	}
+	public void addLocation(int x, int y)
+	{
+		locations.add(x);
+		locations.add(y);
+	}
+	public void setDefaultX(int x)
+	{
+		defaultx = x;
+	}
+	public void setDefaultY(int y)
+	{
+		defaulty=y;
+	}
 }
