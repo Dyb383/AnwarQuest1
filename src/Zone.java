5861af7a06e2f0546ed13c1f72edef153c0bb928
Dylan Brennan <brennand@wit.edu>
2018-04-13 18:33:08 -0400

    finished zone

--- src/Zone.java
+++ src/Zone.java
@@ -2,11 +2,11 @@ import java.awt.Image;
 import java.util.ArrayList;
 
 public class Zone {
-	Image img;
-	int defaultx;
-	int defaulty;
-	ArrayList<Integer> locations = new ArrayList<Integer>();
-	Zone next;
+	private Image img;
+	private int defaultx;
+	private int defaulty;
+	private ArrayList<Integer> locations = new ArrayList<Integer>();
+	private Zone next;
 	public void setImg(Image imgIn)
 	{
 		img = imgIn;
@@ -23,5 +23,29 @@ public class Zone {
 	public void setDefaultY(int y)
 	{
 		defaulty=y;
+	}
+	public void setNext(Zone m)
+	{
+		next = m;
+	}
+	public Image getImg()
+	{
+		return img;
+	}
+	public ArrayList<Integer> getLocations()
+	{
+		return locations;
+	}
+	public int getDefaultX()
+	{
+		return defaultx;
+	}
+	public int getDefaultY()
+	{
+		return defaulty;
+	}
+	public Zone getNext()
+	{
+		return next;
 	}
 }
